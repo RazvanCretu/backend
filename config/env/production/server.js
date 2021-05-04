a2e0c64462e1881d4f3255e957211150753bdce1
Razvy Cretu <razvan.cretu97@gmail.com>
2021-05-05 01:04:20 +0300

    new

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,8 @@
 module.exports = ({ env }) => ({
     url: env('HEROKU_URL'),
+    admin: {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET', '688cb0beb29d8c239ed50b1d4330d16a'),
+        },
+      },
   });
